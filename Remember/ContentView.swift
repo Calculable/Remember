fd2898a53c6dca1218301d02f975ec3299927498
Jan Huber <jan.huber@ost.ch>
2022-08-15 16:30:29 +0200

    Initial Commit

---
+++ Remember/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Remember
+//
+//  Created by Jan Huber on 15.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Remember/RememberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberApp.swift
+//  Remember
+//
+//  Created by Jan Huber on 15.08.22.
+//
+
+import SwiftUI
+
+@main
+struct RememberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
