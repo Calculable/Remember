04d0a44da4e257e21537db7f7df2378b88d80f62
Jan Huber <jan.huber@ost.ch>
2022-08-19 12:20:25 +0200

    Biometric authentication cannot be enabled if the device does not support it

--- Remember/Helper/AuthenticationHelper.swift
+++ Remember/Helper/AuthenticationHelper.swift
@@ -12,12 +12,26 @@ struct AuthenticationHelper {
     
     @Binding var isUnlocked: Bool
     
-    func authenticate() {
+    static func checkIfBiometricsAreAvailable(onSuccess: (() -> Void)?, onError: ((NSError?) -> Void)?) {
         let context = LAContext()
         var error: NSError?
-
+        
         // check whether biometric authentication is possible
         if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            onSuccess?()
+        } else {
+            // no biometrics
+            onError?(error)
+        }
+        
+        
+    }
+    
+    func authenticate() {
+        
+        let context = LAContext()
+        
+        AuthenticationHelper.checkIfBiometricsAreAvailable() {
             // it's possible, so go ahead and use it
             let reason = "We need to unlock your data."
 
@@ -30,10 +44,13 @@ struct AuthenticationHelper {
                     isUnlocked = false
                 }
             }
-        } else {
+        } onError: { _ in
             // no biometrics
             isUnlocked = true //! a note was added to the UI to inform the user that the data will be accessible if biometrics are disabled in the system settings
         }
+
+
+      
     }
     
     
--- Remember/View/Settings/PrivacySettingsView.swift
+++ Remember/View/Settings/PrivacySettingsView.swift
@@ -14,7 +14,7 @@ struct PrivacySettingsView: View {
 
     @AppStorage("biometric.authentication.enabled") private var enableBiometricAuthentication = false
     
-   
+    @State private var showBiometricsNotAvailableAlert = false
     
     var body: some View {
         
@@ -24,19 +24,37 @@ struct PrivacySettingsView: View {
             
             Text("You can use Face ID or Touch ID to unlock the app. Please note that the data will be accessible if biometrics are disabled for this device.")
                 .font(.callout)
+                .alert("Biometrics not available", isPresented: $showBiometricsNotAvailableAlert) {
+                            Button("OK", role: .cancel) { }
+                        } message: {
+                            Text("Biometrics are disabled or not supported on this device")
+                        }
             
             Section {
                 
                 Toggle(isOn: $enableBiometricAuthentication){
                          Text("Enable biometric authentication (if available)")
-                      }
-                
+                }.onChange(of: enableBiometricAuthentication == true , perform: { _ in
+                    checkIfBiometricsAvailable()
+                })
+     
+                 
             }
         }
         .navigationTitle("Privacy")
         .navigationBarTitleDisplayMode(.inline)
     }
+    //checkIfBiometricsAreAvailable
     
+    func checkIfBiometricsAvailable() {
+        if (enableBiometricAuthentication) {
+            AuthenticationHelper.checkIfBiometricsAreAvailable() {} onError: { _ in
+                enableBiometricAuthentication = false
+                showBiometricsNotAvailableAlert = true
+                
+            }
+        }
+    }
 }
 
 struct PrivacySettingsView_Previews: PreviewProvider {
