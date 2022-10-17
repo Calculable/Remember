bba659ba81abd9a95b499f71225d08470fc60288
Jan Huber <>
2022-10-17 06:59:19 +0200

    Setup Unit Tests

---
+++ RememberTests/ExampleTests.swift
@@ -0,0 +1,14 @@
+//
+//  ExampleTests.swift
+//  RememberTests
+//
+//  Created by Jan Huber on 17.10.22.
+//
+
+import XCTest
+
+class ExampleTests: XCTestCase {
+  func testTallestTowersShouldNotBeEmpty() {
+    XCTAssert(1 > 0)
+  }
+}
