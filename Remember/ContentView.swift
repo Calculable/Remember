7f5a99935d537b15edb6a5fa9074438cf2d596a6
Jan Huber <>
2022-11-05 10:12:02 +0100

    remove hot reloading package

--- Remember/ContentView.swift
+++ Remember/ContentView.swift
@@ -89,12 +89,4 @@ class ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-    
-    #if DEBUG
-    @objc class func injected() {
-        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
-        windowScene?.windows.first?.rootViewController =
-        UIHostingController(rootView: ContentView_Previews.previews)
-    }
-    #endif
 }
--- Remember/RememberApp.swift
+++ Remember/RememberApp.swift
@@ -2,17 +2,6 @@ import SwiftUI
 
 @main
 struct RememberApp: App {
-    init() {
-        #if DEBUG
-        var injectionBundlePath = "/Applications/InjectionIII.app/Contents/Resources"
-        #if targetEnvironment(macCatalyst)
-        injectionBundlePath = "\(injectionBundlePath)/macOSInjection.bundle"
-        #elseif os(iOS)
-        injectionBundlePath = "\(injectionBundlePath)/iOSInjection.bundle"
-        #endif
-        Bundle(path: injectionBundlePath)?.load()
-        #endif
-    }
 
     var body: some Scene {
         WindowGroup {
