324423c00885fb934d6cd4cc1c266bf795a146be
Jan Huber <>
2022-11-05 09:48:43 +0100

    Add links to privacy policy and contact form

--- Remember/View/Settings/SettingsView.swift
+++ Remember/View/Settings/SettingsView.swift
@@ -28,21 +28,43 @@ struct SettingsView: View {
         }
     }
     
+    var privacyPolicyNavigationLink: some View {
+        Link(destination: URL(string: "https://www.jan-huber.ch/remember-app/privacy-policy")!, label: {
+            Text("Privacy Policy")
+        })
+    }
+    
+    var contactNavigationLink: some View {
+        Link(destination: URL(string: "https://www.jan-huber.ch/remember-app/contact")!, label: {
+            Text("Contact & Support")
+        })
+    }
+    
     var resetMemoriesNavigationLink: some View {
         NavigationLink {
             ResetMemoriesView()
         } label: {
-            Text("Reset")
+            Text("Reset").foregroundColor(.red)
         }
     }
     
     var body: some View {
         NavigationView {
             List {
-                notificationNavigationLink
-                deletedMemoriesNavigationLink
-                importAndExportNavigationLink
-                resetMemoriesNavigationLink
+                Section {
+                    notificationNavigationLink
+                    deletedMemoriesNavigationLink
+                    importAndExportNavigationLink
+                }
+                
+                Section {
+                    contactNavigationLink
+                    privacyPolicyNavigationLink
+                }
+                
+                Section {
+                    resetMemoriesNavigationLink
+                }
             }
             .navigationTitle("Settings")
         }
